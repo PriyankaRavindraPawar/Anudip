da01d1ee4ce1df080dc964a8b5f1254abcd1e4ad
SAURABH <SAURABH@SAURABH>
2022-11-24 16:36:26 +0530

    commit

--- Assignment_7/src/File_1.java
+++ Assignment_7/src/File_1.java
@@ -1,40 +1,27 @@
-import java.util.*;
-import java.io.*;
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 public class File_1
 {
-		public static void main(String[] args)throws IOException
-		{
-			     File obj=new File("D:\\Anudip\\File_3.txt");
-			     
-			     Scanner sc=new Scanner(obj);
-			     int count=0;
-			     while(sc.hasNextLine())
-			     {
-			    	 String str=sc.nextLine();
-			    	
-			    	 String []arr=str.split(" ");
-				     
-		             count +=arr.length;
-		         
-		    
-				FileInputStream obj1 = new FileInputStream("D:\\Anudip\\File_3.txt");
-				
-				int i;
-				String str1 = " ";
-				
-				while((i=obj1.read())!=-1)   // read data from text file
-				{
-					System.out.print((char)i);
-				}
-				
-				obj1.close();	
-			}
-	     
-		}
-			 
-			     
-		}
-
-
+	public static void main(String[] args) throws IOException {
+     File obj=new File("D:\\Anudip\\File_3.txt");
+    
+     Scanner sc=new Scanner(obj);
+     String st="";
+     String s=sc.nextLine();
+     for(int i=0;i<s.length();i++)
+     {
+    	 if(s.charAt(i)!=' ') 
+    	 {
+    		 st+=s.charAt(i);
+    	 }
+    	 else
+    	 {
+    		if(st.length()>5)
+    			System.out.println(st+" ");
+    		st=" "; 
+    	 }
+     }
+     sc.close();
+	}
+}
