fdca5076b4d3bcb0d58764f61e56b0c6c69a0d6e
SAURABH <SAURABH@SAURABH>
2022-12-01 13:01:46 +0530

    commit

---
+++ Assignment_8/src/Aircraft.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Aircraft {
+
+	public static void main(String[] args) {
+		
+          Scanner sc=new Scanner(System.in);
+          
+          System.out.println("Enter  Total Row = ");
+          int n=sc.nextInt();
+           int sum=0;
+          int a[]=new int[n];
+          System.out.println("Enter row value =");
+          for(int i=0;i<n;i++)
+          {
+            a[i]=sc.nextInt();
+          
+        	sum+=a[i];
+        	
+       //  String result=(n>0)?"Positive":"Negative";
+         //        System.out.println(result);
+          }
+          System.out.println("Real prime numbers : " + Arrays.toString(a));
+          System.out.println(sum);
+         
+          for(int i=0;i<n;i++)
+          {
+          if(a[i]<0)
+     	 {
+            	System.out.println("Invalid Input");
+           }
+}
+}
+}
