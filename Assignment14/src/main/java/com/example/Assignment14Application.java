7544a5170324145e8b8285016a732731eae71e4b
SAURABH <SAURABH@SAURABH>
2023-01-18 12:54:15 +0530

    commit

---
+++ Assignment14/src/main/java/com/example/Assignment14Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment14Application.class, args);
+	}
+
+}
---
+++ Assignment14/src/main/java/com/example/Exception/GroceriesException.java
@@ -0,0 +1,16 @@
+package com.example.Exception;
+
+public class GroceriesException extends Exception {
+	
+
+	public GroceriesException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public GroceriesException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+	
+}
+
